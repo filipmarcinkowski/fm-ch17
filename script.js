76a9da15f484f7c9e6a6a7dbb9e145f6ac3c50ab
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-01-30 02:30:16 +0100

    files cleared

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const qBox = document.querySelectorAll('.faq-box');
+const boxImage = document.querySelector('.image__box');
 
 qBox.forEach(function (el) {
   const answer = el.querySelector('.answer-box');
@@ -7,5 +8,12 @@ qBox.forEach(function (el) {
   el.addEventListener('click', function () {
     answer.classList.toggle('open');
     arrow.classList.toggle('rotate');
+  });
+
+  el.addEventListener('mouseover', function () {
+    boxImage.classList.add('take-box');
+  });
+  el.addEventListener('mouseout', function () {
+    boxImage.classList.remove('take-box');
   });
 });
